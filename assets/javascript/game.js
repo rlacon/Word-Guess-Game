58746e8a796c4611999db78aac2f5f91e1e9dc5d
Ryan Lacon <ryan.lacon@gmail.com>
2019-06-29 10:54:57 -0400

    Cleaned up pseudo code and added function to start game on page load

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,39 +1,49 @@
-
-//For array, nest secondary arrays inside of superheroNames array for each name. Separate each word by string element. e.g.: var firstWord = ["S", "U","P","E","R","M","A","N",];
-
-var superheroNames = ["Superman", "Batman", "Deadpool", "Spiderman", "Thor", "Hulk"];
-var win = 0;
-var currentWord = [];
+var wins = 0;
 var guessesRemaining = 0;
 var lettersGuessed = [];
 
-// The user presses any key to get started
-document.onkeyup = function(event) {
-    var userGuess = event.key;
-    console.log(userGuess);
-    var chosenWord = Math.floor(Math.random() * superheroNames.length);
-    console.log(chosenWord);
-}
+var superheroNames = ["Superman", "Batman", "Deadpool", "Spiderman", "Thor", "Hulk"];
+var chosenWord = [];
 
-// When key pressed...
+// On page load...
+
+function beginGame() {
+    alert("Press a key to guess the word!");
+
 
 //Computer auto picks a random word from array
+var chosenWord = Math.floor(Math.random() * superheroNames.length);
+    console.log(chosenWord);
 
-//Generate underscores in place of chosen word and display on screen
-
-//When key pressed matches a string element, show word in place of underscore
-
-
+//Displays word on screen
 
 
+};
+//Replaces each string element of chosenWord with underscores 
+for (var i = 0; i <= superheroNames.length; i++) {
+}
 
 
+//---------------GAME BEGINS HERE---------------//
 
+// The user presses a key to make a guess
+document.onkeyup = function (event) {
+    var userGuess = event.key;
+    console.log(userGuess);
 
-//If a letter guessed doesn't match, the number of guesses remaining decreases by 1
+};
 
-//If the letter guessed doesn't match, it will appear under "letters already guessed"
+//If key pressed matches string element of chosenWord, replace underscore with word
 
-//If the number of guesses hits 0, an alert message will display saying "You lose" and reset
+//If key pressed doesn't match, decrease number of guesses by -1
 
-//If the complete word is typed, an alert message will display saying "You win" and reset
+    //Add letter to "letters-guessed-text" 
+
+//When number of guesses = 0, display alert message "You Lose!" 
+
+    //Reset guesses-remaining-text and letters-guessed-text
+
+//If chosenWord is fully typed, display alert message "You Win!"
+
+    //Add 1 point to wins counter
+    //Reset guesses-remaining-text and letters-guessed-text
