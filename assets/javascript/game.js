c7850c41998adccf00fd4dad8de44bd91a49d85f
Ryan Lacon <ryan.lacon@gmail.com>
2019-06-29 11:04:15 -0400

    Cleaned up pseudo code and added more context to html

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,11 +1,11 @@
 var wins = 0;
-var guessesRemaining = 0;
-var lettersGuessed = [];
+var guessesRemaining = 10;
 
 var superheroNames = ["Superman", "Batman", "Deadpool", "Spiderman", "Thor", "Hulk"];
 var chosenWord = [];
+var lettersGuessed = [];
 
-// On page load...
+//---------------ON PAGE LOAD---------------//
 
 function beginGame() {
     alert("Press a key to guess the word!");
@@ -15,15 +15,17 @@ function beginGame() {
 var chosenWord = Math.floor(Math.random() * superheroNames.length);
     console.log(chosenWord);
 
-//Displays word on screen
+//Display word on screen
 
 
-};
-//Replaces each string element of chosenWord with underscores 
+
+
+
+//Replace each string element of chosenWord with underscores 
 for (var i = 0; i <= superheroNames.length; i++) {
 }
 
-
+};
 //---------------GAME BEGINS HERE---------------//
 
 // The user presses a key to make a guess
@@ -32,18 +34,30 @@ document.onkeyup = function (event) {
     console.log(userGuess);
 
 };
-
 //If key pressed matches string element of chosenWord, replace underscore with word
 
 //If key pressed doesn't match, decrease number of guesses by -1
 
     //Add letter to "letters-guessed-text" 
 
-//When number of guesses = 0, display alert message "You Lose!" 
+
+
+//---------------LOSE GAME---------------//
+
+//When guessesRemaining = 0, display alert message "You Lose!" 
 
     //Reset guesses-remaining-text and letters-guessed-text
+
+    //Computer chooses new word from array
+
+
+
+//---------------WIN GAME---------------//
 
 //If chosenWord is fully typed, display alert message "You Win!"
 
     //Add 1 point to wins counter
+
     //Reset guesses-remaining-text and letters-guessed-text
+
+    //Computer chooses new word from array
